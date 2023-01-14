2e3c167bec123c92605090486c35a0ab72a5ad18
Edgardo N. Angeles <87631824+Infin8Phy6@users.noreply.github.com>
2023-01-14 14:08:04 +0800

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,6 +1,8 @@
 import math 
 import numpy as np
 import matplotlib.pyplot as plt
+import PySimpleGUI as sg
+
 
 
 
@@ -38,14 +40,75 @@ ax.set_yticks(np.arange(ymin, ymax+1), minor=True)
 #Finally, add a grid.
 ax.grid(which='both', color='grey', linewidth=1, linestyle='-', alpha=0.2)
 
+sg.theme('SandyBeach')	
+
+# Very basic window.
+# Return values using
+# automatic-numbered keys
+layout = [
+	[sg.Text('Please enter your coordinates, Vector1, Vector2')],
+	[sg.Text('V1_x', size =(10, 1)), sg.InputText()],
+	[sg.Text('V1_y', size =(10, 1)), sg.InputText()],
+    [sg.Text('V2_x', size =(10, 1)), sg.InputText()],
+	[sg.Text('V2_y', size =(10, 1)), sg.InputText()],
+	[sg.Submit(), sg.Cancel()]
+]
+window = sg.Window('Simple data entry window', layout)
+event, values = window.read()
+o1=0
+#assign vector values here  for x
+w=o1
+x=o1
+#y=1
+#z=0
+y= float(values[0])
+z= float(values[1])
+dx1=math.sqrt(((y - w)**2)+((z- x)**2))
+#print (dx1)
+#assign vector values here  for y
+a=o1
+b=o1
+#c=0
+#d=1
+c= float(values[2])
+d= float(values[3])
+
+
+d2x=math.sqrt(((c - a)**2)+((d - b)**2))
+#print (d2x)
+
+resultant=math.sqrt(((dx1)**2)+((d2x)**2))
+print ("Vector Lenght =")
+print (resultant)
+
+print ("Angle between the vectors =")
+ang=(math.asin(dx1/resultant))
+angcos=(math.acos(dx1/resultant))
+angle=math.degrees((ang))
+print (angle)
+yterminal=((resultant)*(ang))
+print("y-terminal")
+print (yterminal)
+print("x-terminal")
+xterminal=((resultant)*(angcos))
+print (xterminal)
+
+
+
+
 
 #p.arrow( x, y, dx, dy, **kwargs )
 #we can scale it here
-plt.arrow( 0, 0, 0.0, 1,  fc="k", ec="k", head_width=0.2, head_length=0.1, width=.1 )
-plt.arrow( 0, 0, 1, 0,  fc="red", ec="red", head_width=0.2, head_length=0.1, width=.1)
+plt.arrow( w, x, y, z,  fc="red", ec="red", head_width=0.2, head_length=0.1, width=.1)
+plt.arrow( a, b, c, d,  fc="k", ec="k", head_width=0.2, head_length=0.1, width=.1 )
+plt.arrow( a, b, xterminal, yterminal,  fc="yellow", ec="yellow", head_width=0.2, head_length=0.1, width=.1 )
+
+#add the vectors to yield the resultant vector
 
 
 #plt.quiver(b, y)
 
 plt.show()
+
+
 
